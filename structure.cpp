dca2a4012eb3c6668f0d514b8c8d21d83032d080
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-02-05 22:39:34 +0530

    Create structure.cpp
    Basic structure of c++

---
+++ structure.cpp
@@ -0,0 +1,10 @@
+//this code is to tell basic structure of c++
+#include<iostream> //This is calld a headerfile. In this io means Input output
+using namespace std;//by using this u dont need to put std in front of cout everytime
+int main(){
+
+  cout<<"Hello Everyone";//prints Hello Everyone
+  //std::cout<"Hello Everyone"; //this how we write couut without using 'using namespace std'.
+
+  return 0;
+}
