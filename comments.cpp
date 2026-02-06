7fff769068976044add9579263e55fd37a1b30fa
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-02-06 12:14:34 +0530

    Create comments.cpp
    This is for learning how to use comments in cpp

---
+++ comments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+
+    std::cout<<"comments are lines in our code which do not compile\n";
+    std::cout<<"Double slash is used for a single line comment\n";
+    //This is a singlr line comment
+    std::cout<<"Between /**/ we can write comment in more than one line\n";
+    /*I eat biryani
+    Everybody likes it*/
+
+    return 0;
+}
