d7028c08aae81ab3c7cc606bdd4bf6495280d578
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-02-06 12:54:54 +0530

    Create data_types.cpp
    Useful to understand datatypes and variables in C++

---
+++ data_types.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+  /*In c++ thhere are mainly 5 datatypes. they are
+  1) int 
+  2)float
+  3)double
+  4)char
+  5)boolean*/
+  //the name we use to store a data is called variable. example age, name etc.
+    int age = 6;//int is used for storing integer value like 1,2,-1,10,123 etc
+    int b=7,c=3;//can also declare like this
+    cout<<"The value of b is "<<b<<" the value of c is "<<c;
+    cout<<"\nYour are "<<age<<" years old";
+    float pi = 3.14;//float used to store decimal numbers
+    cout<<"\nThe value of pi is "<<pi;
+    double e = 2.71828;//double is used to long decimal 
+    cout<<"\nThe value of e is "<<e;
+    char d='A';//char is used to store a single character for example 'A' 'a' '@' '$' ';' '1'
+    cout<<"\nThe character is "<<d;
+
+
+    return 0;
+}
