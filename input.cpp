801b8ec78313e4a123647f3db1dd6b917968c2bf
divyacharan-08 <divyacharancheenepalli@gmail.com>
2026-02-08 12:33:07 +0530

    Create input.cpp

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a;
+    cout<<"Enter your age\n";
+    cin>>a; //cin used to take input from the user
+    cout<<"You are "<<a<<" years old";
+    // >> is called extraction operator
+    
+return 0;
+}
